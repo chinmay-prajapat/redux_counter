1629bdf5cea5ddf0fd50bfa6df7b02551d05f5bd
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-17 13:41:16 +0530

    devtool

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import { reducer } from "./reducers";
-
-export const store = createStore(reducer);
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(); //don't it into production
+export const store = createStore(reducer, enhancer);
